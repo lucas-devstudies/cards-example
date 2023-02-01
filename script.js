dfab76cea75348681a041db8416fcdc39b00ee83
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2023-02-01 07:19:47 -0400

    inserindo o javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+const buttonOne = document.querySelector("#btn-card1");
+const buttonTwo = document.querySelector("#btn-card2");
+const buttonThree = document.querySelector("#btn-card3");
+const buttonFour = document.querySelector("#btn-card4");
+const buttonFive = document.querySelector("#btn-card5");
+const buttonSix = document.querySelector("#btn-card6");
+let color2 = '#F8D2A3'
+
+
+buttonOne.addEventListener("click", function () {
+  buttonOne.innerHTML = "conclúido";
+  buttonOne.style.backgroundColor = color2;
+});
+
+buttonTwo.addEventListener("click", function () {
+  buttonTwo.innerHTML = "conclúido";
+  buttonTwo.style.backgroundColor = color2;
+});
+
+buttonThree.addEventListener("click", function () {
+  buttonThree.innerHTML = "conclúido";
+  buttonThree.style.backgroundColor = color2;
+});
+
+buttonFour.addEventListener("click", function () {
+  buttonFour.innerHTML = "conclúido";
+  buttonFour.style.backgroundColor = color2;
+});
+
+buttonFive.addEventListener("click", function () {
+  buttonFive.innerHTML = "conclúido";
+  buttonFive.style.backgroundColor = color2;
+});
+
+buttonSix.addEventListener("click", function () {
+  buttonSix.innerHTML = "conclúido";
+  buttonSix.style.backgroundColor = color2;
+});
